2c35b526475be6f5a24664657243774a12c5e893
marcovicar <marcosvictormbi85@gmail.com>
2025-06-06 23:28:57 +0100

    Updating APP.js divs

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {Helmet} from "react-helmet";
 import {Toaster} from "react-hot-toast";
 import Home from "./pages/home/index.jsx";
 import Navbar from './components/Navbar.jsx';
+import CreateUserPage from "./pages/createUser/index.jsx";
 
 function App() {
 
@@ -23,14 +24,24 @@ function App() {
         {/* TODO: Create sidebar*/}
         <Navbar/>
 
-        <div className={""}>
-          <Routes>
-            <Route
-              path={"/"}
-              element={<Home />}
-            />
-            {/*<Redirect to={`/`} />*/}
-          </Routes>
+        <div className={"h-screen bg-white overflow-hidden flex"}>
+          <div
+            className={
+              `relative flex flex-col flex-1 mx-auto w-0 mb-2 pb-2 transition-transform duration-300 max-w-screen-fullHD md:px-6 z-0`
+            }
+          >
+            <Routes> {/* Old Switch */}
+              <Route
+                path={"/"}
+                element={<Home />}
+              />
+              <Route
+                path={"/create-user"}
+                element={<CreateUserPage />}
+              />
+              {/*<Redirect to={`/`} />*/}
+            </Routes> {/* Old Switch */}
+          </div>
         </div>
       </div>
     </Router>
