7dd9feadb1d3df48b55269d375e76fcb7084f819
marcovicar <marcosvictormbi85@gmail.com>
2025-06-06 23:26:09 +0100

    Add createUserPage

---
+++ src/pages/createUser/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CreateUserPage() {
+
+  return (
+    <div className={"flex justify-center align-middle"}>
+      <h1>Create User</h1>
+    </div>
+  )
+}
