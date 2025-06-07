b7dc8caaac97b3c1239946c0034edc87b1fea25d
marcovicar <marcosvictormbi85@gmail.com>
2025-06-07 17:49:20 +0100

    🌟 Adding component StoresCarousel & FeedPreferences

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MainSectionWrapper from "../../components/MainWrapper/MainWrapper.jsx";
-import SuggestionsToFollow from "../../components/SuggestionsToFollow/SuggestionsToFollow.js";
+import SuggestionsToFollow from "../../components/SuggestionsToFollow/SuggestionsToFollow.jsx";
 import Posts from "../../components/Posts/Posts.jsx";
+import StoriesCarousel from "../../components/StoriesCarousel/StoriesCarousel.jsx";
+import FeedPreferences from "../../components/FeedPreferences/FeedPreferences.jsx";
 
 export default function Home() {
 
@@ -9,6 +11,8 @@ export default function Home() {
     <MainSectionWrapper
       customStyle={"overflow-y-auto scrollbar scrollbar-thin scrollbar-thumb-gray-200 scrollbar-track-gray-100 relative"}
     >
+      <FeedPreferences />
+      <StoriesCarousel />
       <div className={"grid grid-cols-[1fr_300px] gap-4 px-4"}>
         <div
           id={"feed"}
