c898dd77d131625450991bb50ae87e5b3378f2c9
marcovicar <marcosvictormbi85@gmail.com>
2025-06-08 15:50:09 +0100

    Fix on StoriesCarousel using fake data

--- src/components/StoriesCarousel/StoriesCarousel.jsx
+++ src/components/StoriesCarousel/StoriesCarousel.jsx
@@ -1,20 +1,75 @@
 import React from 'react';
 import {shortText} from "../../helpers/globals/index.js";
 
-function Stories() {
+const storiesData = [
+  {
+    id: 0,
+    user: {
+      username: "marcovicar",
+      avatar: {
+        url: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: ""
+      }
+    },
+  },
+  {
+    id: 1,
+    user: {
+      username: "fabiokek",
+      avatar: {
+        url: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: ""
+      }
+    },
+  },
+  {
+    id: 2,
+    user: {
+      username: "viniciuscoelho",
+      avatar: {
+        url: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: ""
+      }
+    },
+  },
+  {
+    id: 3,
+    user: {
+      username: "super_earth",
+      avatar: {
+        url: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: ""
+      }
+    },
+  },
+  {
+    id: 4,
+    user: {
+      username: "im_not_a_bot",
+      avatar: {
+        url: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: ""
+      }
+    },
+  },
+];
+
+function Stories({
+  stories,
+}) {
   return (
     <div className={"flex flex-col gap-1"}>
       <div className="cursor-pointer bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 p-[2px] rounded-full">
         <div className="bg-white p-[2px] rounded-full">
           <img
-            src={"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"}
-            alt={""}
+            src={stories?.user?.avatar.url}
+            alt={stories?.user?.avatar.url || ""}
             className="h-14 w-14 rounded-full object-cover"
           />
         </div>
       </div>
       <span className={"text-[12px] leading-tight"}>
-        {shortText("marcovicar", 9)}
+        {shortText(stories?.user?.username, 9)}
       </span>
     </div>
   )
@@ -23,14 +78,9 @@ function Stories() {
 export default function StoriesCarousel() {
   return (
     <div className={"flex gap-4 pb-8"}>
-      <Stories />
-      <Stories />
-      <Stories />
-      <Stories />
-      <Stories />
-      <Stories />
-      <Stories />
-      <Stories />
+      {storiesData && storiesData?.length > 0 && storiesData.map((item, index) => (
+        <Stories key={index} stories={item} />
+      ))}
     </div>
   );
 }
